63959c147b0b6df8b339deb9631f1e9e8c1492b9
Michelle Steigerwalt <msteigerwalt@gmail.com>
2015-06-12 18:14:57 -0400

    edit-in-place (our first modal component)
    This is an example of how to use the edit-in-place modal to create
    an editable item.
    
    We use a bunch of semantic HTML to make the JavaScript fairly
    reusable, though the strategy to save a particular key/value pair
    might change.  (Keep an eye on later commits for a customization
    strategy.)
    
    Functionality can be observed by going into the Head Items section
    and clicking on an attribute name.

---
+++ static/js/modals/edit-in-place.js
@@ -0,0 +1,26 @@
+Sugar.addModal('edit-in-place', {
+
+	display: function(mode, modal) {
+	
+		// '.edit' -> '.edit-in-place.modal .mode-display .edit'	
+		mode.listen('.edit', 'click', function(evt, parentMode, parentModal) {
+			evt.stop();
+			parentModal.setMode('edit');
+		});
+	
+	},
+
+	edit: function(mode, modal) {
+
+		// 'form' -> '.edit-in-place.modal .mode-edit form'
+		mode.listen('form', 'submit', function(evt, parentMode, parentModal) {
+			evt.stop();
+			var newValue = parentMode.find('input[type=text]').el.value;
+			var display  = parentModal.find('.value');
+			display.el.innerHTML = newValue;
+			parentModal.setMode('display');
+		});
+
+	}
+
+});
