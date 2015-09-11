3eb27f761286706a902e2657db3415b9ca73d091
Nick Wagner <nickwisc@gmail.com>
2015-09-11 10:12:45 -0400

    header
    added header, match master branch...

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask, request, session, jsonify, redirect, url_for, abort, render_template, flash, Response, make_response
 from flask.ext.cache import Cache
 
